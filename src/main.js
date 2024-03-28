5a2b5ce2ae9d6a53446b5ca86293caa22c87b84a
FREEDOMTSQ <2643106302@qq.com>
2024-03-28 20:08:13 +0800

    router初始编写

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import router from './router';
 
 
 import App from './App.vue'
@@ -11,5 +12,6 @@ Vue.config.productionTip = false
 Vue.use(ElementUI);
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Home from '../views/Home'
+import User from '../views/User'
 
 Vue.use(VueRouter)
+
+const routes = [
+    {
+        path: '/home',
+        component: Home
+    },
+    {
+        path: '/user',
+        component: User
+    }
+
+]
+
+const router = new VueRouter({
+    routes
+})
+
+export default router
