2f84865b2561d46eac994aef07b6cddd94d1ebdd
FREEDOMTSQ <2643106302@qq.com>
2024-03-29 11:40:30 +0800

    基础边栏菜单静态数据

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,15 +1,26 @@
 <template>
   <div>
-    <h1>Main页面</h1>
-    <router-view></router-view>
+    <el-container>
+      <el-aside width="200px">
+        <common-aside />
+      </el-aside>
+      <el-container>
+        <el-header>
+          <h1>Main页面</h1>
+        </el-header>
+        <el-main>
+          <router-view></router-view>
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 <script>
+import CommonAside from '../components/CommonAside.vue'
 export default {
+  components:{ CommonAside},
   data() {
-    return {
-      
-    }
+    return {};
   },
-}
+};
 </script>
